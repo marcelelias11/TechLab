7dd301ee454ba95c774de3c2d02f854032d31b9d
Marcelelias11 <marcelelias11@gmail.com>
2024-12-02 17:52:08 -0300

    Desenvolvimento do back

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -7,7 +7,7 @@ const mysql = require("mysql2");
 server.use(cors());
 server.use(express.json());
 server.use(express.static("src"));
-const connection = mysql.createConnection({
+const db = mysql.createConnection({
   host: "mysql-d5f7afc-marcelelias11-5b03.k.aivencloud.com",
   port: "19351",
   user: "avnadmin",
@@ -20,9 +20,9 @@ const connection = mysql.createConnection({
 });
 
 dotenv.config();
-connection.connect();
+db.connect();
 
-connection.query(`SELECT * FROM equipes`, (err, rows, fields) => {
+db.query(`SELECT * FROM equipes`, (err, rows, fields) => {
   if (err) throw err;
   console.log(rows);
 });
@@ -76,26 +76,51 @@ server.post("/sim", (req, res) => {
 });
 
 server.post("/dbstat", (req, res) => {
-  fetch("http://localhost:5000/stat", {
-    method: "POST",
-    mode: "cors",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(req.body),
-  })
-    .then(async function (response) {
-      console.log(response);
-      return await response.json();
-    })
-    .then(async function (text) {
-      console.log(text);
-      res.status(201).send(text);
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+  if (res.body.team === 0) {
+    db.query(
+      `SELECT idemail FROM estudantesemail WHERE email = '${req.body.email};`,
+      (err, rows, fields) => {
+        if (err) throw err;
+        console.log(rows);
+        let idemail = rows[0];
+        db.query(
+          `SELECT idestudante FROM estudantes WHERE estudantesemail_idemail = '${idemail}';`,
+          (err, rows, fields) => {
+            if (err) throw err;
+            console.log(rows);
+            let idestudante = rows[0];
+            db.query(
+              `SELECT estatistica_has_pesquisa_estatistica_idestatistica FROM estudantes_has_equipe WHERE estudantes_idestudante = '${idestudante}';`,
+              (err, rows, fields) => {
+                if (err) throw err;
+                console.log(rows);
+                let idestatistica = rows[0];
+                if (res.body.mode === "load") {
+                  db.query(
+                    `SELECT * FROM estatistica WHERE idestatistica = '${idestatistica}';`,
+                    (err, rows, fields) => {
+                      if (err) throw err;
+                      console.log(rows);
+                      res.status(201).send(rows);
+                    }
+                  );
+                } else if (res.body.mode === "save") {
+                  console.log(req.body.values);
+                  db.query(
+                    `INSERT INTO estatistica VALUES '${req.body.values}';`,
+                    (err, rows, fields) => {
+                      if (err) throw err;
+                      res.status(201).send("Success!");
+                    }
+                  );
+                }
+              }
+            );
+          }
+        );
+      }
+    );
+  }
 });
 
 server.listen(8080, () => {
